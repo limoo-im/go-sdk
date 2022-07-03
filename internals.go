1a5e568fa69a0123783075536c913e45122937bb
Muhammed Hussein Karimi <info@karimi.dev>
2022-07-04 01:27:34 +0430

    some clean-ups and internal request handler

---
+++ internals.go
@@ -0,0 +1,75 @@
+package sdk
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/limoo-im/go-sdk/types"
+	log "github.com/sirupsen/logrus"
+)
+
+// login to limoo and return the new token
+func (c *LimooClient) login() error {
+	body := fmt.Sprintf("j_username=%v&j_password=%v", c.Username, c.Password)
+	res, err := c.httpClient.Post(c.BaseURL+"/Limonad/j_spring_security_check", "application/x-www-form-urlencoded", strings.NewReader(body))
+	if err != nil {
+		return err
+	}
+	var resError error
+	if res.StatusCode == http.StatusUnauthorized {
+		resError = errors.New("bad username or password, check your credentials")
+		return resError
+	} else if res.StatusCode != http.StatusOK {
+		resError = errors.New("unknown status code from server")
+		return resError
+	}
+	respbody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return err
+	}
+	var loginResp types.LoginResult
+	err = json.Unmarshal(respbody, &loginResp)
+	if err != nil {
+		return err
+	}
+	token := res.Header.Get("Token")
+	c.lastToken = &token
+	// TODO: Log response body
+	log.WithField("event", "login").Debugf("Headers: %v", res.Header)
+	return nil
+}
+
+// a function that is used to send all of the requests (except for the login) and login again if gets Unauthorized status
+func (c *LimooClient) do(uri string, method, contentType string, body io.Reader) (*http.Response, error) {
+	url, err := url.Parse(c.BaseURL + uri)
+	if err != nil {
+		return nil, err
+	}
+	request := &http.Request{
+		Method: method,
+		URL:    url,
+		Body:   io.NopCloser(body),
+	}
+	request.Header.Set("Authorization", fmt.Sprintf("Bearer %s", *c.lastToken))
+	request.Header.Set("Content-Type", contentType)
+	response, err := c.httpClient.Do(request)
+	if response.StatusCode == http.StatusUnauthorized {
+		err = c.login()
+		if err != nil {
+			return nil, err
+		}
+		response, err = c.httpClient.Do(request)
+		if err != nil {
+			return nil, err
+		}
+	} else if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unknown status %v code from server", response.StatusCode)
+	}
+	return response, nil
+}
--- sdk.go
+++ sdk.go
@@ -4,11 +4,8 @@ import (
 	"bytes"
 	"crypto/tls"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"strings"
 
 	"github.com/limoo-im/go-sdk/types"
 	log "github.com/sirupsen/logrus"
@@ -51,58 +48,20 @@ func (c *LimooClient) New(limooBaseURL, username, password string, insecureSkipV
 			InsecureSkipVerify: insecureSkipVerify,
 		},
 	}
-	token, err := c.login()
-	c.lastToken = token
-	return err
-}
-
-// login to limoo and return the new token
-// TODO: use refresh token
-func (c *LimooClient) login() (*string, error) {
-	body := fmt.Sprintf("j_username=%v&j_password=%v", c.Username, c.Password)
-	res, err := c.httpClient.Post(c.BaseURL+"/Limonad/j_spring_security_check", "application/x-www-form-urlencoded", strings.NewReader(body))
-	if err != nil {
-		return nil, err
-	}
-	var resError error
-	if res.StatusCode == http.StatusUnauthorized {
-		resError = errors.New("bad username or password, check your credentials")
-		return nil, resError
-	} else if res.StatusCode != http.StatusOK {
-		resError = errors.New("unknown status code from server")
-		return nil, resError
-	}
-	respbody, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-	var loginResp types.LoginResult
-	err = json.Unmarshal(respbody, &loginResp)
-	if err != nil {
-		return nil, err
-	}
-	token := res.Header.Get("Token")
-	// TODO: Log response body
-	log.WithField("event", "login").Debugf("Headers: %v", res.Header)
-	return &token, nil
+	return c.login()
 }
 
 // send a message to a conversation
-// TODO: return a readable response and also login if necessary
+// TODO: return a readable response
 func (c *LimooClient) SendMessage(opts types.SendMessageOptions) error {
 	body, err := json.Marshal(opts)
 	if err != nil {
 		return err
 	}
-	var resError error
 	reqUri := fmt.Sprintf("/Limonad/api/v1/workspace/items/%v/conversation/items/%v/message/items", opts.WorkspaceID, opts.ConversationID)
-	res, err := c.httpClient.Post(c.BaseURL+reqUri, "application/json", bytes.NewReader(body))
-	if res.StatusCode == http.StatusUnauthorized {
-		resError = errors.New("bad username or password, check your credentials")
-		return resError
-	} else if res.StatusCode != http.StatusOK {
-		resError = errors.New("unknown status code from server")
-		return resError
+	res, err := c.do(reqUri, http.MethodPost, "application/json", bytes.NewReader(body))
+	if err != nil {
+		return err
 	}
 	// TODO: Log response body
 	log.WithField("event", "send message").Debugf("Headers: %v", res.Header)

09fa75b23a149d11db2cb5b1337fe9965e39b6ff
Muhammed Hussein Karimi <info@karimi.dev>
2022-07-04 14:54:43 +0430

    SendMessageOptions can support any message

--- types/types.go
+++ types/types.go
@@ -22,7 +22,26 @@ type LoginResult struct {
 }
 
 type SendMessageOptions struct {
-	WorkspaceID    string `json:"-"`
-	ConversationID string `json:"-"`
-	Text           string `json:"text"`
+	WorkspaceID          string  `json:"-"`
+	ConversationID       string  `json:"-"`
+	Text                 string  `json:"text"`
+	UserID               *string `json:"user_id"`
+	CreateAt             *int    `json:"create_at"`
+	UpdateAt             *int    `json:"update_at"`
+	ThreadRootID         *string `json:"thread_root_id"`
+	DirectReplyMessageID *string `json:"direct_reply_message_id"`
+	PendingMessageID     *string `json:"pending_message_id"`
+	Files                []Files `json:"files"`
+	Props                *string `json:"props"`
+	TimelineLabel        *string `json:"timeline_label"`
+	Type                 *string `json:"type"`
+}
+
+type Files struct {
+	Hash     string            `json:"hash"`
+	Size     int               `json:"size"`
+	Name     string            `json:"name"`
+	MimeType string            `json:"mime_type"`
+	CreateAt int               `json:"create_at"`
+	Metadata map[string]string `json:"metadata"`
 }
